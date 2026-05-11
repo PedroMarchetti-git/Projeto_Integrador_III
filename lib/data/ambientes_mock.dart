29caf044b9385c2dd3ea42004d28f62aca7c0eec
PedroMarchetti-git <pedro.hnm1@puccampinas.edu.br>
2026-05-11 19:45:49 -0300

    Update ambientes_mock.dart with accurate location data and descriptions for environments

--- lib/data/ambientes_mock.dart
+++ lib/data/ambientes_mock.dart
@@ -6,9 +6,44 @@ const List<Ambiente> ambientes = [
     id: "1",
     nome: "Auditório",
     descricao: "Local onde o apagão começou.",
-    latitude: -22.753,
-    longitude: -47.414,
-    raioMetros: 5,
+    latitude: -22.833215,
+    longitude: -47.053100,
+    raioMetros: 10,
   ),
 
+  Ambiente(
+    id: "2",
+    nome: "Biblioteca",
+    descricao: "Lugar silencioso, onde a bibliotecária trabalha.",
+    latitude: -22.833810,
+    longitude: -47.051927,
+    raioMetros: 10,
+  ),
+
+  Ambiente(
+    id: "3",
+    nome: "Praça de Alimentação",
+    descricao: "Lugar geralmente cheio, onde as conversas se espalham.",
+    latitude: -22.833050,
+    longitude:  -47.052015,
+    raioMetros: 10,
+  ),
+
+  Ambiente(
+    id: "4",
+    nome: "Centro de Estudos Africanos e Afro-Brasileiros",
+    descricao: "Algo de estranho aconteceu aqui, dizem que o professor de história viu algo suspeito.",
+    latitude:  -22.833656,
+    longitude: -47.052039,
+    raioMetros: 10,
+  ),
+
+  Ambiente(
+    id: "5",
+    nome: "Manacás",
+    descricao: "Deve ter algum aparelho que possa ter causado o apagão, ou alguém que tenha visto algo.",
+    latitude: -22.833400,
+    longitude: -47.052500,
+    raioMetros: 10,
+  )
 ];
