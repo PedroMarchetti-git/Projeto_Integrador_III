a65437556d88068590ed244b7f14ef94e4420ffe
PedroMarchetti-git <pedro.hnm1@puccampinas.edu.br>
2026-06-01 19:37:52 -0300

    feat: add flutter foundation import to realtime_service.dart

--- lib/realtime_service.dart
+++ lib/realtime_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 
 class RealtimeService {
   final FirebaseDatabase _db = FirebaseDatabase.instance;
